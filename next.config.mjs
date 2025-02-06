5ac918ccebcd62992658b35965d68ed59f1fc352
Hector Oviedo <nosis_r8@hotmail.com>
2025-02-05 22:58:46 -0300

    Fix: add env

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  env: {
+    OLLAMA_URL: process.env.OLLAMA_URL || '$(OLLAMA_URL)',
+  },
   webpack: (config, { isServer }) => {
-        // Fixes npm packages that depend on `fs` module
-        if (!isServer) {
-          config.resolve.fallback = {
-            ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
-              // by next.js will be dropped. Doesn't make much sense, but how it is
-            fs: false, // the solution
-            module: false,
-            perf_hooks: false,
-          };
-        }
-    
-        return config
-      },
-      typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-      },
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+        // by next.js will be dropped. Doesn't make much sense, but how it is
+        fs: false, // the solution
+        module: false,
+        perf_hooks: false,
+      };
+    }
+
+    return config
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 
